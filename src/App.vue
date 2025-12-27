0dddb34946b60f89e505e72ab6d27a03c64f96c9
Hasrat Prawez <hasrat.parwez@tech4logic.com>
2025-12-27 15:31:32 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import UserJourneyView from './pages/UserJourneyView.vue'
+</script>
+
+<template>
+  <UserJourneyView />
+</template>
+
+<style>
+/* Ensure full viewport height */
+#app {
+  @apply h-screen overflow-hidden;
+}
+
+/* Global styles */
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
